51ab0afa1d13effd226d9e0f661e6b67c46c6764
Dan Newman <danlnewman@gmail.com>
2020-07-31 21:42:43 -0700

    Spinner and texture

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public Rigidbody spinny;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spinny.AddRelativeTorque(0f, 0f, 100f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
